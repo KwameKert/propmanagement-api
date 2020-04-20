27170878a35bac917ece625b183de040f3165380
kwamkert <tomriddle477@gmail.com>
2020-04-20 22:32:04 +0000

    First commit

---
+++ src/main/java/com/codeinsyt/tigerseal/TigerSealApplication.java
@@ -0,0 +1,13 @@
+package com.codeinsyt.tigerseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerSealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerSealApplication.class, args);
+	}
+
+}

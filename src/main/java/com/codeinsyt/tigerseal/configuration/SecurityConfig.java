88a050d77c505d561b450e2700446e60a712ad4f
kwamkert <tomriddle477@gmail.com>
2020-04-20 23:53:36 +0000

    Security Cofig updated

--- src/main/java/com/codeinsyt/tigerseal/configuration/SecurityConfig.java
+++ src/main/java/com/codeinsyt/tigerseal/configuration/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.codeinsyt.tigerseal.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,6 +15,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

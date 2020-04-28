995c0906ce9394ebf0f647f8a33ae2319b448090
kwamkert <tomriddle477@gmail.com>
2020-04-28 19:40:31 +0000

    Propertyownerservice added

---
+++ src/main/java/com/codeinsyt/tigerseal/services/interfaces/PropertyOwnerService.java
@@ -0,0 +1,19 @@
+package com.codeinsyt.tigerseal.services.interfaces;
+
+import com.codeinsyt.tigerseal.models.PropertyOwner;
+
+import java.util.HashMap;
+
+public interface PropertyOwnerService {
+
+    public HashMap<String, Object> createOwner(PropertyOwner propertyOwner);
+
+    public HashMap<String, Object> updateOwner(PropertyOwner propertyOwner);
+
+    public HashMap<String, Object> listOwners();
+
+    public HashMap<String, Object> softDelete(Long id);
+
+
+
+}

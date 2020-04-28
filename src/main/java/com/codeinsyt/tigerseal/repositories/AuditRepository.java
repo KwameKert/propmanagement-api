fb182d25628cc44963d54d1d782d471e53617d5d
kwamkert <tomriddle477@gmail.com>
2020-04-28 19:35:50 +0000

    Audit and invoice repositories added

---
+++ src/main/java/com/codeinsyt/tigerseal/repositories/AuditRepository.java
@@ -0,0 +1,7 @@
+package com.codeinsyt.tigerseal.repositories;
+
+import com.codeinsyt.tigerseal.models.Audit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditRepository extends JpaRepository<Audit, Long> {
+}

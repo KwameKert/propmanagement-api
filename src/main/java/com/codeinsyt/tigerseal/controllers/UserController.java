25958c246976195d6ea5e5579b1f4d2633db1013
kwamkert <tomriddle477@gmail.com>
2020-04-28 20:25:16 +0000

    add user method wired in controller

---
+++ src/main/java/com/codeinsyt/tigerseal/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.codeinsyt.tigerseal.controllers;
+
+
+import com.codeinsyt.tigerseal.models.User;
+import com.codeinsyt.tigerseal.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    public ResponseEntity<?> addUser(@Valid User user){
+        return new ResponseEntity<>(this.userService.createUser(user), HttpStatus.OK);
+    }
+}

bded4f6f7b78b735759027e5b0907088013ebc40
kwamkert <tomriddle477@gmail.com>
2020-04-28 19:54:04 +0000

    Invoice services added

---
+++ src/main/java/com/codeinsyt/tigerseal/services/interfaces/InvoiceService.java
@@ -0,0 +1,17 @@
+package com.codeinsyt.tigerseal.services.interfaces;
+
+import com.codeinsyt.tigerseal.models.Invoice;
+
+import java.util.HashMap;
+
+public interface InvoiceService {
+
+
+    public HashMap<String, Object> createInvoice(Invoice invoice );
+
+    public HashMap<String, Object> updateInvoice(Invoice invoice);
+
+    public HashMap<String, Object> listInvoices();
+
+    public HashMap<String, Object> softDelete(Long id);
+}
---
+++ src/main/java/com/codeinsyt/tigerseal/services/interfaces/PropertyService.java
@@ -0,0 +1,17 @@
+package com.codeinsyt.tigerseal.services.interfaces;
+
+
+import com.codeinsyt.tigerseal.models.Property;
+
+import java.util.HashMap;
+
+public interface PropertyService {
+
+    public HashMap<String, Object> createProperty(Property property);
+
+    public HashMap<String, Object> updateOwner(Property property);
+
+    public HashMap<String, Object> listProperties();
+
+    public HashMap<String, Object> softDelete(Long id);
+}

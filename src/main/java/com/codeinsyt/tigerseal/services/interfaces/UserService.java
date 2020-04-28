946f90db3f07a753db69958e3c4d901e1b673ffe
kwamkert <tomriddle477@gmail.com>
2020-04-28 19:58:01 +0000

    user service updated

--- src/main/java/com/codeinsyt/tigerseal/services/interfaces/UserService.java
+++ src/main/java/com/codeinsyt/tigerseal/services/interfaces/UserService.java
@@ -1,8 +1,19 @@
 package com.codeinsyt.tigerseal.services.interfaces;
 
+import com.codeinsyt.tigerseal.models.PropertyOwner;
 import com.codeinsyt.tigerseal.models.User;
+
+import java.util.HashMap;
 
 public interface UserService {
 
     User findByUsernameAndPassword(String username, String password);
+
+    public HashMap<String, Object> createUser(User user);
+
+    public HashMap<String, Object> updateUser(User user);
+
+    public HashMap<String, Object> listUsers();
+
+    public HashMap<String, Object> softDelete(Long id);
 }

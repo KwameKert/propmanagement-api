8c5b0c3f62606389b09c20f60eea394bce629301
kwamkert <tomriddle477@gmail.com>
2020-04-28 19:31:06 +0000

    propert owner added

---
+++ src/main/java/com/codeinsyt/tigerseal/repositories/PropertyOwnerRepository.java
@@ -0,0 +1,7 @@
+package com.codeinsyt.tigerseal.repositories;
+
+import com.codeinsyt.tigerseal.models.PropertyOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyOwnerRepository extends JpaRepository<PropertyOwner, Long> {
+}

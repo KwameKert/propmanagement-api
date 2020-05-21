08e5185a7d7807ded6858436edfdeb3c2438cc68
kwamkert <tomriddle477@gmail.com>
2020-05-21 01:48:24 +0000

    transaction controller completed

--- src/main/java/com/codeinsyt/tigerseal/controllers/TransactionController.java
+++ src/main/java/com/codeinsyt/tigerseal/controllers/TransactionController.java
@@ -1,4 +1,37 @@
 package com.codeinsyt.tigerseal.controllers;
 
+import com.codeinsyt.tigerseal.DTO.PropertyDTO;
+import com.codeinsyt.tigerseal.DTO.TransactionDTO;
+import com.codeinsyt.tigerseal.services.interfaces.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/transaction/")
 public class TransactionController {
+
+
+    private TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addTransaction(@Valid @RequestBody TransactionDTO transactionDTO){
+        return new ResponseEntity<>(this.transactionService.createTransaction(transactionDTO), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listTransaction(){
+        return new ResponseEntity<>(this.transactionService.listAllTransactions(), HttpStatus.OK);
+    }
+
+
+
 }

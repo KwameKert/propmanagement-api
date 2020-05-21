c9c0afb9f3b6e0cf7806e059cbcf555f8b91e345
kwamkert <tomriddle477@gmail.com>
2020-05-21 01:05:06 +0000

    transaction repo updated

--- src/main/java/com/codeinsyt/tigerseal/repositories/TransactionRepository.java
+++ src/main/java/com/codeinsyt/tigerseal/repositories/TransactionRepository.java
@@ -3,7 +3,10 @@ package com.codeinsyt.tigerseal.repositories;
 import com.codeinsyt.tigerseal.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
+        List<Transaction> findByUserId(Long id);
 
 }

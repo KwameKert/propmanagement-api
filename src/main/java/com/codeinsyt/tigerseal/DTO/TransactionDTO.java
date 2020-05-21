82cde224ede91d09eba6ec4f6df7430b9c894641
kwamkert <tomriddle477@gmail.com>
2020-05-21 00:59:58 +0000

    transaction dto added

---
+++ src/main/java/com/codeinsyt/tigerseal/DTO/TransactionDTO.java
@@ -0,0 +1,48 @@
+package com.codeinsyt.tigerseal.DTO;
+
+public class TransactionDTO {
+
+    private Long id;
+
+    private double amount;
+
+    private String method;
+
+    private Long invoiceId;
+
+
+    public TransactionDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public Long getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Long invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+}

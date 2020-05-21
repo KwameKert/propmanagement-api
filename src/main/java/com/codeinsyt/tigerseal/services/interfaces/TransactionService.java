c265472feceae2ce81535b338190013078e2852b
kwamkert <tomriddle477@gmail.com>
2020-05-21 01:06:49 +0000

    transaction service added

--- src/main/java/com/codeinsyt/tigerseal/services/interfaces/TransactionService.java
+++ src/main/java/com/codeinsyt/tigerseal/services/interfaces/TransactionService.java
@@ -1,6 +1,18 @@
 package com.codeinsyt.tigerseal.services.interfaces;
 
+import com.codeinsyt.tigerseal.DTO.TransactionDTO;
+
+import java.util.HashMap;
+
 public interface TransactionService {
+
+
+    public HashMap<String, Object> createTransaction(TransactionDTO transactionDTO);
+
+    public HashMap<String, Object> listAllTransactions();
+
+    public HashMap<String, Object> listUserTransaction();
+
 
 
 }
